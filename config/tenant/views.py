64f197884a233b1fbe3a9443af6025cb7b75426b
Kamohelo <kamohelotshabalala@proton.me>
2026-01-29 18:09:17 +0200

    Add custom queryset filer in tenant/views.py for filtering and pagination

--- config/tenant/views.py
+++ config/tenant/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
-
 from .models import Tenant
 from .serializers import TenantSerializer
 
@@ -10,3 +9,11 @@ class TenantCreateAPIView(generics.ListCreateAPIView):
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
     permission_classes = [AllowAny]
+
+    # Custom query filter
+    def get_queryset(self):
+        queryset = self.queryset
+        name_filter = self.request.query_params.get('name', None)
+        if name_filter is not None:
+            queryset = queryset.filter(name__icontains=name_filter)
+        return queryset
