f0a42ff643bacdfca68e75ce736b0741e9fe62b3
Kamohelo <kamohelotshabalala@proton.me>
2026-01-29 15:49:20 +0200

    Add Tenant viewss in tenant/views.py

--- config/tenant/views.py
+++ config/tenant/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Tenant
+from .serializers import TenantSerializer
 
 # Create your views here.
+class TenantAPIView(generics.ListCreateAPIView):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
