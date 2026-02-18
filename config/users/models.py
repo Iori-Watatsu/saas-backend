69468eef32e4934df2c5325180134a37f66f5bdb
Kamohelo <kamohelotshabalala@proton.me>
2026-02-19 00:43:40 +0200

    Update Custom user model with

--- config/users/models.py
+++ config/users/models.py
@@ -170,6 +170,9 @@ class CustomUser(AbstractUser):
         help_text=_('User must change password on next login')
     )
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name', 'tenant']
 
